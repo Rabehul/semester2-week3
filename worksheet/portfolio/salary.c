5e448211808e8a22ba5ea6aace8284ffed3ae094
Rabeh ul haq islam <qjfm0867@leeds.ac.uk>
2026-03-18 13:38:36 +0000

    salary.c

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,21 +3,36 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Rabehul haq islam
+ * ID: 201972837
  */
 
  int main( void ) {
 
     // define and initialise variables for the problem data 
+    float total_salary = 36250;
+    float NI = 8;
+    float NI_deduction = 0;
+    float tax = 15;
+    float tax_deduction = 0;
+    float salary;
 
     // calculate the deductions and final take-home salary
+   NI_deduction = total_salary * (NI / 100);
+   salary = total_salary - NI_deduction;
+   salary = salary - NI_deduction;
+   if (salary > 12500) {
+      tax_deduction = salary - 12500;
+      tax_deduction = tax_deduction * (tax / 100);
+   }
+   salary = salary - tax_deduction;
+
 
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+    printf("Salary £%.2f\n",total_salary);
+    printf("NI contribution £%.2f\n",NI_deduction);
+    printf("Tax contribution £%.2f\n",tax_deduction);
+    printf("Take home salary £%.2f\n",salary);
 
     return 0;
  }
